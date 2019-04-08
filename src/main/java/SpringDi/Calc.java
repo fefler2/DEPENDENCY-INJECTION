662285f96b2b5f726687baa79d4b3ccbbb15d5ca
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-08 21:30:29 +0200

    DEPENDENCY INJECTION!!!

--- src/main/java/SpringDi/Calc.java
+++ src/main/java/SpringDi/Calc.java
@@ -2,7 +2,7 @@ package SpringDi;
 
 import org.springframework.stereotype.Service;
 
-@Service
+@Service // musi byc @Service bo inaczej nie zadziala @Autowired
 public class Calc {
     int add(int a, int b){
         return a+b;
